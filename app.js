9f59a2d99335125413d696d5a803db9b7157ebfc
Blessed Yahaya <yahayaofficial@gmail.com>
2019-05-26 16:20:35 +0000

    Third commit

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require("express");
 var app = express();
-app.set("view engine", "ejs");
 
+app.set("view engine", "ejs");
+app.use(express.static((__dirname + "/public")));
 
 
 app.get("/", function(req, res){
    res.render("index");
 });
+
+
+
+  
+
 
 
 
