b551c9e1d94d55b73136f8e36cc17207bed4dd80
Blessed Yahaya <yahayaofficial@gmail.com>
2019-05-26 16:03:57 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var app = express();
+
+
+
+app.set("view engine", "ejs");
+app.use(express.static((__dirname + "/public")));
+
+app.get("/", function(req, res){
+   res.render("payNow");
+});
+
+
+
+  
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server is Up");
+});
+
